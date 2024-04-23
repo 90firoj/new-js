de49fe3aad0cb8b239a597fbf14dbd3c2108f895
90firoj <113149881+90firoj@users.noreply.github.com>
2024-04-23 19:17:42 +0000

    datatype

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // use al js code as newer version
+
+//alert(3+3) // we are using node js , not browser
+
+console.log(3 + 
+    3) // code readability should be high
+
+let name ="Firoj "
+let age = 20
+let loggedIn = true 
+
+// number => 2 to power 53
+//bigint
+//string =>""
+//boolean => true or false
+//null => standalone value 
+//undefined => 
+//symbol =>
+
+//object
+
+console.log(typeof "Firoj");
+console.log(typeof age);
+console.log(typeof null); // object type
