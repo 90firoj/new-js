b538c7a067348c2515c762fe9b2067319808d57e
90firoj <113149881+90firoj@users.noreply.github.com>
2024-04-23 18:53:26 +0000

    01_variables

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId =140913
+let accountEmail ="sk123@gmail.com"
+var accountPassword = "1234567"
+accountCity = "Sodepur"
+let accountState ;
+
+//accountId = 2 // not allowed
+
+accountEmail = "hc@gmail.com"
+accountPassword = "098"
+accountCity = "Debagram"
+
+//do not use var it has drawback in block scope and functional scope
+
+console.log(accountId);
+console.table([accountId , accountEmail , accountPassword , accountCity , accountState])
