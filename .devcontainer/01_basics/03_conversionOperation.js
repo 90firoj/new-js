ed3e61204fe712a8657564a4c83ee10ec5df4c84
90firoj <113149881+90firoj@users.noreply.github.com>
2024-04-24 03:59:47 +0000

    Conversion of datatypes

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+//let score = "40abc"
+
+//let score = null
+
+//let score = undefined
+
+/*let score =true
+
+console.log(typeof score);
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);*/
+
+// "40" => 40
+// "40abc" => NaN (Not a Number)
+//true => 1 ; false => 0
+
+let IsLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(IsLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1 =>true ; 0=> false
+//"" =>false
+//"sgfjh" => true
