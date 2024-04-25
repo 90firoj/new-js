3ce65d68a5dbe58ef1615c553a023dbde320a0de
90firoj <113149881+90firoj@users.noreply.github.com>
2024-04-25 12:08:37 +0000

    summary

--- .devcontainer/01_basics/datatype-summary.js
+++ .devcontainer/01_basics/datatype-summary.js
@@ -5,7 +5,7 @@ Reference type or non-primitive :
  array , objects , Functions 
  
  */
-const Id=Symbol('123');
+/* const Id=Symbol('123');
 const anotherId=Symbol('123')
 console.log(Id===anotherId);
 
@@ -21,3 +21,18 @@ const Myfunction = function(){
 }
 
 console.log(typeof sub);
+*/
+
+//+++++++++++++++++++++++++++++++++MEMORY+++++++++++++++++++++++++++++
+
+//memory=> 1. Stack (use for primitive) 2. Heap (use for Non-primitive)
+
+let userOne ={
+    Email:"Firoj@one.com",
+    Id:"123456",
+}
+let userTwo=userOne;
+
+userTwo.Email="Firoj@google.com";
+console.log(userOne.Email);
+console.log(userTwo.Email);
