6af84dc27e0ca01c3af144ec867a38b27910c883
90firoj <113149881+90firoj@users.noreply.github.com>
2024-04-25 12:37:45 +0000

    strings

---
+++ .devcontainer/01_basics/05_strings.js
@@ -0,0 +1,9 @@
+let name="Firoj Sk"
+let repocount=10;
+console.log(`My name is ${name} and repo count is ${repocount}`);
+
+const game=new String("Pokemon")
+console.log(game[3]);
+console.log(game.length);
+console.log(game.charAt(2));
+console.log(game.indexOf("k"));
