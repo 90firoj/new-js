b514899275912fd9be91dfd464ffbb8cad6401ad
90firoj <113149881+90firoj@users.noreply.github.com>
2024-04-26 11:35:44 +0000

    string

--- .devcontainer/01_basics/05_strings.js
+++ .devcontainer/01_basics/05_strings.js
@@ -7,3 +7,14 @@ console.log(game[3]);
 console.log(game.length);
 console.log(game.charAt(2));
 console.log(game.indexOf("k"));
+
+// const substring=game.substring(0,4)
+// console.log(substring);
+
+console.log(game.substring(0,4));
+console.log(game.slice(0,4));
+console.log(game.slice(-6,4));
+
+const strone = "    Firoj    "
+console.log(strone);
+console.log(strone.trim());
